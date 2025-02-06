66a3c75841c0f72fbde7efe32563ab4f7da8372e
Sanjay Suthar <106748519+SanjaySuthar19@users.noreply.github.com>
2025-02-06 14:55:45 +0530

    change: app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,27 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'CRUD API';
+  getRoutes(): any {
+    return {
+      information: "CRUD API - NestJs",
+      routes:{
+        getAll: {
+          route: "/task",
+          method: "GET"
+        },
+        create: {
+          route: "/task",
+          method: "POST"
+        },
+        update: {
+          route: "/task/:id",
+          method: "PATCH"
+        },
+        delete: {
+          route: "/task/:id",
+          method: "DELETE"
+        }
+      }
+    };
   }
 }
